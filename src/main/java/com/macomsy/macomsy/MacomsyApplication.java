9151347b92aa66b507bc81fb7c94199b57be3e9b
Jaime Andres V <jaimeandresvelasqueza@gmail.com>
2024-03-13 21:30:32 -0500

    primer commit

---
+++ src/main/java/com/macomsy/macomsy/MacomsyApplication.java
@@ -0,0 +1,13 @@
+package com.macomsy.macomsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacomsyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacomsyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/macomsy/macomsy/MacomsyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macomsy.macomsy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacomsyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
