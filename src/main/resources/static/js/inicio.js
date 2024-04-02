5172f6ed46529f5b8eb71bc89b51fdce7b2693a9
Santiago <santiagotaborda66@gmail.com>
2024-04-01 19:49:47 -0500

    se hace adelanto de las vistas del inicio, activos, instalaciones

--- src/main/resources/static/js/inicio.js
+++ src/main/resources/static/js/inicio.js
@@ -1,19 +1,35 @@
 document.getElementById("btn_open").addEventListener("click", open_close_menu);
-/* declarar variables*/
-var side_menu=document.getElementById("menu_side");
-var btn_open=document.getElementById("btn_open");
-var body=document.getElementById("body");
-var title_header=document.getElementById("title_header");
 
-/*evento para mostrar y cocultar menu */
+var side_menu = document.getElementById("menu_side");
+var btn_open = document.getElementById("btn_open");
+var body = document.getElementById("body");
+var title_header = document.getElementById("title_header");
 
-function open_close_menu(){
-    body.classList.toggle("body_move")
-    side_menu.classList.toggle("menu__side__move")
+function open_close_menu() {
+    body.classList.toggle("body_move");
+    side_menu.classList.toggle("menu__side__move");
 
-    if(body.className.match("body_move")){
-        title_header.classList.add("show_title") 
-    }else{
-        title_header.classList.remove("show_title")
+    if (body.classList.contains("body_move")) {
+        title_header.classList.add("show_title");
+    } else {
+        title_header.classList.remove("show_title");
     }
 }
+
+document.querySelector('.profile-icon').addEventListener('click', function() {
+    var profileOptions = document.querySelector('.profile-options');
+    profileOptions.style.display = (profileOptions.style.display === 'block') ? 'none' : 'block';
+});
+
+document.querySelector('.settings').addEventListener('click', function() {
+    alert('Opción de configuración seleccionada');
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var cerrarSesionLink = document.getElementById('logout');
+    
+    cerrarSesionLink.addEventListener('click', function(event) {
+        event.preventDefault();
+        window.location.href = 'login.html';
+    });
+});
